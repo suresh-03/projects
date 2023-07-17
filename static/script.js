c36bfdf539c52f7de45ed57f90f98dd765fc597a
suresh-03 <sureshkumar732003@gmail.com>
2023-07-17 10:00:48 +0530

    side bar added

--- static/script.js
+++ static/script.js
@@ -2,22 +2,28 @@ function remove(){
     document.getElementById("msg").remove();
 }
 
+open = false;
+
+function decide(){
+    if(open){
+        closeBar();
+        open = false;
+    }
+    else{
+        show();
+        open = true;
+    }
+}
+
 function show(){
     var sideBar = document.createElement('div');
-    var close = document.createElement("span");
-    close.innerText = "close";
-    close.style.cursor = "pointer";
-    close.style.position = "relative";
-    close.style.left = "60px";
-    // close.onclick = closeBar(sideBar);
-    close.style.marginTop = "90px";
-    sideBar.appendChild(close);
     sideBar.id = "s-bar";
-    sideBar.style.width = "200px";
+    sideBar.style.width = "230px";
     sideBar.style.height = "900px";
     sideBar.style.position = "absolute";
     sideBar.style.zIndex = "4";
-    sideBar.style.top = "0px";
+    sideBar.style.top = "65px";
+    sideBar.style.borderTopRightRadius = "10px";
     document.getElementById("body").style.overflow = "hidden";
     window.scroll({
         top: 0, 
@@ -26,6 +32,12 @@ function show(){
       });
     sideBar.style.backgroundColor = "#efefef";
     document.getElementById("side-bar").appendChild(sideBar);
+}
+
+function closeBar(){
+    document.getElementById('s-bar').remove();
+    document.getElementById('body').style.overflow = "auto";
+    open = false;
 }
 
 
