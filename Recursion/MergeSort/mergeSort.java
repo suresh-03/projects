eaedaf84649af5e89192aac4abc50f701e3cb6c5
suresh-03 <sureshkumar732003@gmail.com>
2023-08-07 20:46:39 +0530

    merge sort

--- Recursion/MergeSort/mergeSort.java
+++ Recursion/MergeSort/mergeSort.java
@@ -1,69 +1,54 @@
-package MergeSort;
-
-
-
 import java.util.Arrays;
 
-public class mergeSort {
-    public static void main(String[] args) {
-        int[] arr = {9,8,7,6,5,4,3,2,1};
+public class mergeSort{
+    public static void main(String[] args){
+        int[] arr = {11,10,9,8,7,6,6,5,4,3,2,1};
         arr = mergesort(arr);
         System.out.println(Arrays.toString(arr));
     }
+
     static int[] mergesort(int[] arr){
         if(arr.length == 1){
             return arr;
         }
+         int mid = arr.length / 2;
 
-        int mid = arr.length / 2;
+         int[] left = mergesort(Arrays.copyOfRange(arr,0,mid));
+         int[] right = mergesort(Arrays.copyOfRange(arr,mid,arr.length));
 
-        int[] left = mergesort(Arrays.copyOfRange(arr, 0, mid));
-        int[] right = mergesort(Arrays.copyOfRange(arr, mid, arr.length));
-
-        return merge(left,right);
+         return merge(left,right);
     }
 
-    static int[] merge(int[] first, int[] second){
-
-        int[] mix = new int[first.length + second.length];
+    static int[] merge(int[] left,int[] right){
+        int[] mix = new int[left.length+right.length];
 
         int i = 0;
         int j = 0;
         int k = 0;
 
-        while(i < first.length && j < second.length){
-            if(first[i] < second[j]){
-                mix[k] = first[i];
+        while(i < left.length && j < right.length){
+            if(left[i] < right[j]){
+                mix[k] = left[i];
                 i++;
             }
             else{
-                mix[k] = second[j];
+                mix[k] = right[j];
                 j++;
             }
             k++;
         }
 
-        // will work if the one of the array has more elements when other array is 
-        // less elements for comparision
-        /*
-         * ex:
-         * a1 = [1,2]
-         * b1 = [3,4,5,6]
-         * 
-         * here 5 and 6 are extra that are added to the mix array
-         */
-
-        while(i < first.length){
-            mix[k] = first[i];
-            k++;
+        while(i < left.length){
+            mix[k] = left[i];
             i++;
-        }
-        while(i < first.length){
-            mix[k] = second[j];
             k++;
+        }
+
+        while(j < right.length){
+            mix[k] = right[j];
             j++;
+            k++;
         }
         return mix;
-        
     }
 }
