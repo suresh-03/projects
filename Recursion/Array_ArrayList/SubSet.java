68ca4a89b87a25f3f1a61dd8bb5893ab14edebda
suresh-03 <sureshkumar732003@gmail.com>
2023-08-10 20:43:56 +0530

    SubSet

---
+++ Recursion/Array_ArrayList/SubSet.java
@@ -0,0 +1,31 @@
+package Array_ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubSet {
+	public static void main(String[] args) {
+		int[] arr = {1,2,3};
+		System.out.println(subset(arr));
+	}
+
+	static List<List<Integer>> subset(int[] arr) {
+		List<List<Integer>> outer = new ArrayList<>();
+
+		outer.add(new ArrayList<>());
+
+		for (int num : arr) {
+			int size = outer.size();
+			for (int i = 0; i < size; i++) {
+				List<Integer> internal = new ArrayList<>(outer.get(i));
+				internal.add(num);
+				outer.add(internal);
+			}
+
+		}
+
+		outer.remove(0);
+
+		return outer;
+	}
+}
